a4f8a9c5b29a6d0011d567de9a5d5cef4a525610
Hugo Alonso Luis <10271728+halonsoluis@users.noreply.github.com>
2024-08-25 12:03:56 +0200

    Remove possible unused fields to avoid extra parsing

--- geosnap/geosnap/Networking/FlickrPhotoResponse.swift
+++ geosnap/geosnap/Networking/FlickrPhotoResponse.swift
@@ -5,25 +5,25 @@ import Foundation
 
 struct FlickrPhotoResponse: Decodable {
     let photos: Photos
-    let stat: String
+    //  let stat: String
 }
 
 struct Photos: Decodable {
-    let page: Int
-    let pages: Int
-    let perpage: Int
-    let total: String
+    //   let page: Int
+    //   let pages: Int
+    //   let perpage: Int
+    //   let total: String
     let photo: [Photo]
 }
 
 struct Photo: Decodable {
     let id: String
-    let owner: String
+    //  let owner: String
     let secret: String
     let server: String
     let farm: Int
-    let title: String
-    let ispublic: Int
-    let isfriend: Int
-    let isfamily: Int
+    //   let title: String
+    //   let ispublic: Int
+    //  let isfriend: Int
+    //   let isfamily: Int
 }
