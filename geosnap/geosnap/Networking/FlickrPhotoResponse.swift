217ff52ed22ee8fb13dfe7668a41d5a837587425
Hugo Alonso Luis <10271728+halonsoluis@users.noreply.github.com>
2024-08-25 12:01:59 +0200

    Move flickr response to production

---
+++ geosnap/geosnap/Networking/FlickrPhotoResponse.swift
@@ -0,0 +1,29 @@
+// Created for geosnap in 2024
+// Using Swift 5.0
+
+import Foundation
+
+struct FlickrPhotoResponse: Decodable {
+    let photos: Photos
+    let stat: String
+}
+
+struct Photos: Decodable {
+    let page: Int
+    let pages: Int
+    let perpage: Int
+    let total: String
+    let photo: [Photo]
+}
+
+struct Photo: Decodable {
+    let id: String
+    let owner: String
+    let secret: String
+    let server: String
+    let farm: Int
+    let title: String
+    let ispublic: Int
+    let isfriend: Int
+    let isfamily: Int
+}
--- geosnap/geosnapTests/FlickrPhotoResponseTests.swift
+++ geosnap/geosnapTests/FlickrPhotoResponseTests.swift
@@ -2,58 +2,7 @@
 // Using Swift 5.0
 
 import XCTest
-
-/*
- Result of call.
-
- {
-     "photos": {
-         "page": 1,
-         "pages": 11,
-         "perpage": 1,
-         "total": 11,
-         "photo": [
-             {
-                 "id": "16250914286",
-                 "owner": "25304693@N00",
-                 "secret": "35e6795da4",
-                 "server": "8608",
-                 "farm": 9,
-                 "title": "The results of the Belgian Weather :-)",
-                 "ispublic": 1,
-                 "isfriend": 0,
-                 "isfamily": 0
-             }
-         ]
-     },
-     "stat": "ok"
- }
- */
-
-struct FlickrPhotoResponse: Decodable {
-    let photos: Photos
-    let stat: String
-}
-
-struct Photos: Decodable {
-    let page: Int
-    let pages: Int
-    let perpage: Int
-    let total: String
-    let photo: [Photo]
-}
-
-struct Photo: Decodable {
-    let id: String
-    let owner: String
-    let secret: String
-    let server: String
-    let farm: Int
-    let title: String
-    let ispublic: Int
-    let isfriend: Int
-    let isfamily: Int
-}
+@testable import geosnap
 
 final class FlickrPhotoResponseTests: XCTestCase {
 
