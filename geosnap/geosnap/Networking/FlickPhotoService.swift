8e998edd91a149ab52e7b2ff374f45d9643c13a8
Hugo Alonso Luis <10271728+halonsoluis@users.noreply.github.com>
2024-08-25 11:43:33 +0200

    Improve readability of the composited url

--- geosnap/geosnap/Networking/FlickPhotoService.swift
+++ geosnap/geosnap/Networking/FlickPhotoService.swift
@@ -16,11 +16,25 @@ struct FlickrPhotoService {
     private let apiKey = "6a704fa13e809d5b0e65010950af64d2"  //TODO: Change API key and keep this in a safe place as a secret
 
     func fetchPhoto(latitude: Double, longitude: Double, completion: @escaping (Result<Data, Error>) -> Void) {
-        let baseURL = "https://www.flickr.com/services/rest/"
-        let parameters = "?method=flickr.photos.search&api_key=\(apiKey)&lat=\(latitude)&lon=\(longitude)&radius=0.1&radius_units=km&per_page=1&page=1&content_type=0&media=photos&has_geo=1&format=json&nojsoncallback=1"
-        let urlString = "\(baseURL)\(parameters)"
+        var urlComponents = URLComponents(string: "https://www.flickr.com/services/rest/")!
 
-        guard let url = URL(string: urlString) else {
+        urlComponents.queryItems = [
+            URLQueryItem(name: "method", value: "flickr.photos.search"),
+            URLQueryItem(name: "api_key", value: apiKey),
+            URLQueryItem(name: "lat", value: "\(latitude)"),
+            URLQueryItem(name: "lon", value: "\(longitude)"),
+            URLQueryItem(name: "radius", value: "0.1"),
+            URLQueryItem(name: "radius_units", value: "km"),
+            URLQueryItem(name: "per_page", value: "1"),
+            URLQueryItem(name: "page", value: "1"),
+            URLQueryItem(name: "content_type", value: "0"),
+            URLQueryItem(name: "media", value: "photos"),
+            URLQueryItem(name: "has_geo", value: "1"),
+            URLQueryItem(name: "format", value: "json"),
+            URLQueryItem(name: "nojsoncallback", value: "1")
+        ]
+
+        guard let url = urlComponents.url else {
             completion(.failure(PhotoError.invalidURL))
             return
         }
